8c9fd3acf7d1aa9437497bbb9b56baf76c87c216
Oboo <oboosarpi@gmail.com>
2021-08-19 23:48:56 +0000

    Migrated the database

--- log_learning/settings.py
+++ log_learning/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-_mx1agie3448z3@(!7qrjd(fyt=n5&g8m@$&g#!agg+1+2oeaq'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['127.0.0.1', 'note-today.herokuapp.com']
 
